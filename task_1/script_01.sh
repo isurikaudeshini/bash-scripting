34b04b29786ab499d53b32661514a270a25dbf90
Isurika Wickramasinghe <isurika.ak@gmail.com>
2024-08-15 09:52:30 +0000

    defined variables in script_01

--- task_1/script_01.sh
+++ task_1/script_01.sh
@@ -3,10 +3,12 @@
 #
 echo -e "This script prints the 6th & 10th column of a 10 column CSV.\n"
 
-COLUMN_COUNT=$(awk -F ',' '{print NF; exit}' trade.csv)
+csv_file=trade.csv
+
+COLUMN_COUNT=$(awk -F ',' '{print NF; exit}' $csv_file)
 
 if [ "$COLUMN_COUNT" -eq 10 ]; then
-        awk -F ',' '{OFS=","; print $6, $10}'  trade.csv
+        awk -F ',' '{OFS=","; print $6, $10}'  $csv_file
     else
         echo "File does not contain 10 columns!"
 fi  
