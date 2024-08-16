c3421653864b288e382c4031dfc5505658d47aac
Isurika Wickramasinghe <isurika.ak@gmail.com>
2024-08-16 07:35:25 +0000

    script_01  modified

--- task_1/script_01.sh
+++ task_1/script_01.sh
@@ -3,14 +3,22 @@
 #
 echo -e "This script prints the 6th & 10th column of a 10 column CSV.\n"
 
-csv_file=trade.csv
 
-COLUMN_COUNT=$(awk -F ',' '{print NF; exit}' $csv_file)
 
-if [ "$COLUMN_COUNT" -eq 10 ]; then
-        awk -F ',' '{OFS=","; print $6, $10}'  $csv_file
+# Check if number of columns are greater than or equal 10 & print 6th & 10th column
+
+prefix_adding() {
+
+COLUMN_COUNT=$(awk -F ',' '{print NF; exit}' $1)
+
+if [ "$COLUMN_COUNT" -ge 10 ]; then
+        awk -F ',' '{OFS=","; print $6, $10}'  $1
     else
         echo "File does not contain 10 columns!"
 fi  
 
+}
 
+csv_file="./csv_files/trade.csv"
+
+prefix_adding "$csv_file"
